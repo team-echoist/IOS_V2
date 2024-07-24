c09d346cf8e9a4bc64a4ce25ebd72e5a27265d0c
Kevin Lee <leesangha123@naver.com>
2024-07-24 15:02:44 +0900

    Initial Commit

---
+++ LinkedOut/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LinkedOut
+//
+//  Created by 이상하 on 7/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
