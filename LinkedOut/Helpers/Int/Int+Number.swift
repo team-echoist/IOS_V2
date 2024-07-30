bf5f57faa2f38bac33b4962962c403456a809c6f
Kevin Lee <leesangha123@naver.com>
2024-07-30 11:01:43 +0900

    [Add] Int extension 추가

---
+++ LinkedOut/Helpers/Int/Int+Number.swift
@@ -0,0 +1,25 @@
+//
+//  Int+Number.swift
+//  LinkedOut
+//
+//  Created by 이상하 on 7/30/24.
+//
+
+import Foundation
+import UIKit
+
+extension Int {
+    var f: CGFloat { return CGFloat(self) }
+}
+
+extension Float {
+    var f: CGFloat { return CGFloat(self) }
+}
+
+extension Double {
+    var f: CGFloat { return CGFloat(self) }
+}
+
+extension CGFloat {
+    var swf: Float { return Float(self) }
+}
